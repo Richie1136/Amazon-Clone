75046d8b10cc7f8e778282920ab17471c52d0a43
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-01-17 13:36:19 -0500

    Added a firebase.js file which has all the firebase information that i need

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCsUwzHAVb1x2wr0cn-tGUji3-fiAroFIA",
+  authDomain: "clone-9b302.firebaseapp.com",
+  projectId: "clone-9b302",
+  storageBucket: "clone-9b302.appspot.com",
+  messagingSenderId: "462236359739",
+  appId: "1:462236359739:web:c6801cc4e728780bf9d01e"
+};
