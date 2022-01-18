2009de74c0b823e7c29dde55a028e49bf7554e88
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-01-18 16:34:24 -0500

    Working on the different routes in the application

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import { Route, Navigate, Routes, Link } from 'react-router-dom'
+import Cart from './components/cart/Cart';
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
+      <Routes>
+        <Route path='/' element={<><Header /><Home /></>} />
+        <Route path='/checkout' element={<><Header /><Cart /></>} />
+      </Routes>
     </div>
   );
 }
