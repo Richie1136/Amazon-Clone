22d58291b3395d527dcd80bc9658d39a9f647eb8
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-01-18 16:41:14 -0500

    Working on the Checkout component

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import { Route, Navigate, Routes, Link } from 'react-router-dom'
-import Cart from './components/cart/Cart';
+import Checkout from './components/checkout/Checkout';
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <Routes>
-        <Route path='/' element={<><Header /><Home /></>} />
-        <Route path='/checkout' element={<><Header /><Cart /></>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/checkout' element={<Checkout />} />
       </Routes>
     </div>
   );
---
+++ amazon-clone/src/components/checkout/Checkout.js
@@ -0,0 +1,11 @@
+import './Checkout.css'
+
+const Checkout = () => {
+  return (
+    <div className='checkout'>
+      <h1>Checkout</h1>
+    </div>
+  )
+}
+
+export default Checkout
