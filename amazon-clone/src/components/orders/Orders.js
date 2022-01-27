af3fa742440c3b625771f4debf6d7c8562dfa3b8
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-01-27 16:15:59 -0500

    Displaying all of my orders that i have submitted to the data base

--- amazon-clone/src/components/orders/Orders.js
+++ amazon-clone/src/components/orders/Orders.js
@@ -2,6 +2,7 @@ import './Orders.css'
 import { useState, useEffect } from 'react'
 import { db } from '../../firebase';
 import { useStateValue } from '../../store/state-context';
+import Order from '../order/Order';
 
 
 
@@ -11,20 +12,29 @@ const Orders = () => {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    db.collection('users').doc(user?.uid)
-      .collection('orders').orderBy('created', 'desc')
-      .onSnapshot(snapshot => {
-        setOrders(snapshot.docs.map(doc => ({
-          id: doc.id,
-          data: doc.data()
-        })))
-      })
-  }, []);
+    if (user) {
+      db.collection('users').doc(user?.uid)
+        .collection('orders').orderBy('created', 'desc')
+        .onSnapshot(snapshot => {
+          setOrders(snapshot.docs.map(doc => ({
+            id: doc.id,
+            data: doc.data()
+          })))
+        })
+    } else {
+      setOrders([])
+    }
+  }, [user]);
 
 
   return (
     <div className='orders'>
       <h1>Your Orders</h1>
+      <div className='orders-order'>
+        {orders.map(order => {
+          return <Order order={order} />
+        })}
+      </div>
     </div>
   )
 };
