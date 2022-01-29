96fd4c9afcfbc157eb165dcbc004d3e73895077d
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-01-29 10:34:05 -0500

    Made change

--- amazon-clone/functions/index.js
+++ amazon-clone/functions/index.js
@@ -31,7 +31,7 @@ app.post("/payments/create", async (request, response) => {
   const paymentIntent = await stripe.paymentIntents.create({
     amount: total,
     currency: 'usd',
-  })
+  });
   // OK - Created
   response.status(201).send({
     clientSecret: paymentIntent.client_secret,
