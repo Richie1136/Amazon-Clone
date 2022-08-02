ece221e9b8abd592bc292f4bedea32234465076c
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-08-02 13:54:53 -0400

    Updated stripe package

--- amazon-clone/src/index.js
+++ amazon-clone/src/index.js
@@ -1,21 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
-import { StateProvider } from './store/state-context'
-import cartReducer, { initialState } from './reducer'
+import { BrowserRouter } from 'react-router-dom';
+import { StateProvider } from './store/state-context';
+import cartReducer, { initialState } from './reducer';
+import './index.css'
 
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <StateProvider initialState={initialState} reducer={cartReducer}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StateProvider>,
-  document.getElementById('root')
+  </StateProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
